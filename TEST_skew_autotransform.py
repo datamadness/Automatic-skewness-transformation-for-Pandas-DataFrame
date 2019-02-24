13a4c7566c2c93941224a83d8535cd3bd5f99d13
datamadness <39350541+datamadness@users.noreply.github.com>
2019-02-23 17:57:01 -0800

    Initial commit
    Adding the python funtion and
    test script

---
+++ TEST_skew_autotransform.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 23 17:10:57 2019
+
+@author: DATAmadness
+"""
+
+############################################
+###### TEST for skew autotransform##########
+
+import pandas as pd
+import numpy as np
+from skew_autotransform import skew_autotransform
+
+#Import test dataset - Boston huosing data
+from sklearn.datasets import load_boston
+
+exampleDF = pd.DataFrame(load_boston()['data'], columns = load_boston()['feature_names'].tolist())
+
+transformedDF = skew_autotransform(exampleDF.copy(deep=True), plot = True, exp = True, threshold = 0.5)
+
+print('Original average skewness value was %2.2f' %(np.mean(abs(exampleDF.skew()))))
+print('Average skewness after transformation is %2.2f' %(np.mean(abs(transformedDF.skew()))))
---
+++ skew_autotransform.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 23 14:42:46 2019
+
+@author: DATAmadness
+"""
+
+##################################################
+# A function that will accept a pandas dataframe
+# and auto-transforms columns that exceeds threshold value
+#  -  Offers choice between boxcox or log / exponential transformation
+#  -  Automatically handles negative values
+#  -  Auto recognizes positive /negative skewness
+
+
+
+
+
+
+import seaborn as sns
+import numpy as np
+import math
+import scipy.stats as ss
+import matplotlib.pyplot as plt
+
+def skew_autotransform(DF, include = None, exclude = None, plot = False, threshold = 1, exp = False):
+    colnames = DF.columns.values
+    
+    #Helper function that checks if all values are positive
+    def make_positive(series):
+        minimum = np.amin(series)
+        
+        #If minimum is negative, offset all values by a constant to move all values to positive teritory
+        if minimum <= 0:
+            series = series + minimum + 0.01
+        return series
+    
+    
+    #Go throug desired columns in DataFrame
+    for col in colnames:
+        #Get column skewness
+        skew = DF[col].skew()
+        transformed = True
+        
+        if plot:
+            #Prep the plot of original data
+            sns.set_style("darkgrid")
+            sns.set_palette("Blues_r")
+            fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+            ax1 = sns.distplot(DF[col], ax=axes[0])
+            ax1.set(xlabel='Original ' + col)
+        
+        #If skewness is larger than threshold and positively skewed; If yes, apply appropriate transformation
+        if abs(skew) > threshold and skew > 0:
+            skewType = 'positive'
+            #Make sure all values are positive
+            DF[col] = make_positive(DF[col])
+            
+            if exp:
+               #Apply log transformation 
+               DF[col] = DF[col].apply(math.log)
+            else:
+                #Apply boxcox transformation
+                DF[col] = ss.boxcox(DF[col])[0]
+            skew_new = DF[col].skew()
+         
+        elif abs(skew) > threshold and skew < 0:
+            skewType = 'negative'
+            #Make sure all values are positive
+            DF[col] = make_positive(DF[col])
+            
+            if exp:
+               #Apply exp transformation 
+               DF[col] = DF[col].pow(10)
+            else:
+                #Apply boxcox transformation
+                DF[col] = ss.boxcox(DF[col])[0]
+            skew_new = DF[col].skew()
+        
+        else:
+            #Flag if no transformation was performed
+            transformed = False
+            skew_new = skew
+        
+        #Compare before and after if plot is True
+        if plot:
+            print('\n ------------------------------------------------------')     
+            if transformed:
+                print('\n %r had %r skewness of %2.2f' %(col, skewType, skew))
+                print('\n Transformation yielded skewness of %2.2f' %(skew_new))
+                sns.set_palette("Paired")
+                ax2 = sns.distplot(DF[col], ax=axes[1], color = 'r')
+                ax2.set(xlabel='Transformed ' + col)
+                plt.show()
+            else:
+                print('\n NO TRANSFORMATION APPLIED FOR %r . Skewness = %2.2f' %(col, skew))
+                ax2 = sns.distplot(DF[col], ax=axes[1])
+                ax2.set(xlabel='NO TRANSFORM ' + col)
+                plt.show()
+                
+
+    return DF
+
+
+
+
+
